94cbe6b84bf89bf09989e7353f40b7046a9695c5
Prasetyo Katon <prasetyokaton10@gmail.com>
2025-05-24 15:50:32 +0700

    pindahin lokasi json

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def fill_gender(df):
 def apply_media_tier_logic(df):
     try:
         # === SETUP GOOGLE SHEETS API ===
-        SERVICE_ACCOUNT_FILE = 'phase1\.secretcontainer\insightsautomation-460807-acdad1ee7590.json'
+        SERVICE_ACCOUNT_FILE = '/home/insights-vps/.secretcontainer/insightsautomation-460807-acdad1ee7590.json'
         SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
         creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
         client = gspread.authorize(creds)
